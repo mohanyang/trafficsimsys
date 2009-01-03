39f558a081789ebf7f57c2919327fcc2173264bf
hsunshine <hsunshine@eff4231e-b49b-11dd-b56c-6ddcb7750b1e>
2009-01-03 02:12:31 +0000

    fixed several bugs in calculating intersection points; used lazy insertion in newPoint of newRoad;

--- trunk/src/traffic/simulation/kernel/Simulator.java
+++ trunk/src/traffic/simulation/kernel/Simulator.java
@@ -47,6 +47,9 @@ public class Simulator {
 		sWin.setRatio(0.4);
 		map = new LoadHandler().load();
 		sWin.setRatio(0.6);
+		Log.getInstance().writeln("Point list");
+		for (Iterator<Point> p = map.getPointList(); p.hasNext();)
+			Log.getInstance().writeln(p.next().toString());
 		Log.getInstance().writeln("Vehicle list");
 		sWin.setRatio(0.8);
 		for (Iterator<Point> p = map.getPointList(); p.hasNext();)
